fbb9af87d048d1991d0814356edb651beba129b7
mufidat3250 <wahabmufidat1919@gmail.com>
2023-01-14 22:37:13 +0100

    fixes

--- src/organism/Footer/index.tsx
+++ src/organism/Footer/index.tsx
@@ -8,15 +8,15 @@ const Footer = () => {
     <div className="footerWraper ">
      <div className="footer-content">
      <div className="flex  gap-[1rem]">
-      <div className="">
+      <div className="hidden md:block">
       <FooterLogo />
       </div>
         <div>
-          <i><p className="max-w-[12rem] text-[13px] font-[400] ">21 Oceanview Terrace Montauk, NY 11954 631 — 668 — 2050</p></i>
+          <i><p className="max-w-[12rem] text-[10px] md:text-[13px] font-[400] ">21 Oceanview Terrace Montauk, NY 11954 631 — 668 — 2050</p></i>
           <i>hello @ marrammontauk . com</i>
         </div>
       </div>
-      <div className="max-w-[17rem] grid grid-cols-2 text-[14px] text-right space-x-[1rem]  text-white font-bold">
+      <div className="max-w-[17rem]  text-[10px] grid grid-cols-2 md:text-[14px] text-right space-x-[1rem]  text-white font-bold">
             {link.map((link, index)=> <a className="no-wrap">{link}</a>)}
       </div>
      </div>
