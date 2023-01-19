7a1e29af290ee9b30e7bc62c8ad8f794f29207f6
mufidat3250 <wahabmufidat1919@gmail.com>
2023-01-19 16:00:18 +0100

    fixes

--- src/molecules/Happenings/index.tsx
+++ src/molecules/Happenings/index.tsx
@@ -2,15 +2,23 @@ import './Hapenings.scss'
 import {happenings} from './../../data'
 import Menu from '../../attom/Menu.tsx'
 import { accordionImg } from '../../entity/accordion'
+import { useEffect } from 'react'
 
 const Happenings = () => {
   const swtichImg = accordionImg.use(); 
-  // console.log(happenings[swtichImg].img, 'check')
+  console.log(happenings[swtichImg].img, 'check' , swtichImg)
+  let img;
+  useEffect(() => {
+    img= happenings[swtichImg].img
+    console.log(img, 'useeffect')
+  
+  }, [])
+  
    return (
     <div className='happenings-wrapper' id='happenings'>
         <h1>HAPPENINGS</h1>
         <div className='tourism'>
-            {/* <img src={happenings[swtichImg].img} alt="" /> */}
+            <img src={img} alt="" />
             <div>
             {happenings.map((data, index)=> <Menu data={data} key={`menu--${index}`} lastIndex={index=== happenings.length-1}/>)}
             </div>
