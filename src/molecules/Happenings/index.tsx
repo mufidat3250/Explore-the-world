75006e6c73f2b7fa8482e789be642706a1cb0fd5
mufidat3250 <wahabmufidat1919@gmail.com>
2023-01-19 16:14:12 +0100

    fixes

--- src/molecules/Happenings/index.tsx
+++ src/molecules/Happenings/index.tsx
@@ -7,18 +7,13 @@ import { useEffect } from 'react'
 const Happenings = () => {
   const swtichImg = accordionImg.use(); 
   console.log(happenings[swtichImg].img, 'check' , swtichImg)
-  let img;
-  useEffect(() => {
-    img= happenings[swtichImg].img
-    console.log(img, 'useeffect')
-  
-  }, [])
+
   
    return (
     <div className='happenings-wrapper' id='happenings'>
         <h1>HAPPENINGS</h1>
         <div className='tourism'>
-            <img src={'/images/happening-7.webp '} alt="" />
+            <img src={'/images/happening-7.webp '|| happenings[swtichImg].img} alt="" />
             <div>
             {happenings.map((data, index)=> <Menu data={data} key={`menu--${index}`} lastIndex={index=== happenings.length-1}/>)}
             </div>
